3650ece481ed99f7083c0ed3e0e319454770e8bc
Jessica Rock <e1530023@cmaisonneuve.qc.ca>
2018-02-08 12:48:22 -0500

    Nettoyage du code

--- 02_serveur.js
+++ 02_serveur.js
@@ -5,7 +5,6 @@ app.use(express.static('public'));
 
 app.get('/formulaire', function (req, res) {
  console.log(__dirname);
- //res.sendFile( __dirname + "/" + "01_form.html" );
 
  fs.readFile( __dirname + "/public/html/" + "01_form.html", 'utf8', function (err, data) {
  console.log( data );
@@ -15,7 +14,6 @@ app.get('/formulaire', function (req, res) {
 
 app.get('/membres', (req, res) => {
  fs.readFile( __dirname + "/public/data/" + "adresses.json", 'utf8', function (err, data) {
- //console.log( data );
  let json = JSON.parse(data);
  res.end(transforme_en_tableau(json));
  });
@@ -28,10 +26,11 @@ app.get('/', (req, res) => {
 })
 
 
+
+
 let reponse = {};
 
 app.get('/traiter_get', function (req, res) {
- // Preparer l'output en format JSON
 
 console.log('la route /traiter_get')
 
@@ -44,96 +43,23 @@ console.log('la route /traiter_get')
  courriel:req.query.courriel
  };
 
- console.log(reponse);
- //reponse = JSON.stringify(reponse);
-
-
 
 fs.readFile( __dirname + "/public/data/" + "adresses.json", 'utf8', function (err, data) {
- if (err) throw err;
- let tab = JSON.parse(data);
- tab.push(reponse);
- let json = JSON.stringify(tab);
-
- fs.writeFile(__dirname + "/public/data/" + "adresses.json", json, function(err, data) {
 	if (err) throw err;
- 	console.log('Terminé');
- })
+	let tab = JSON.parse(data);
+	tab.push(reponse);
+	let json = JSON.stringify(tab);
 
- res.end();
+	 fs.writeFile(__dirname + "/public/data/" + "adresses.json", json, function(err, data) {
+		if (err) throw err;
+	 	console.log('Terminé');
+	 })
+
  });
 
-
-
-
-
-/*
- let membres = fs.readFileSync('public/data/adresses.json');
- JSON.stringify(membres);
-fs.appendFile('unfichier.txt', membres + ',' + reponse, function (err) {
-  if (err) throw err;
-  console.log('Sauvegardé');
-});
-
-
- let nouveauMembre = fs.readFileSync('unfichier.txt');
- JSON.parse('[' + nouveauMembre + ']');
- console.log('++++++++++');
- console.log(nouveauMembre);*/
-
- /*
-
-fs.appendFile('ajout.txt', ',' + JSON.stringify(reponse), function(err){
-		if(err) throw err;
-})
-
-*/
-
- fs.writeFile ("public/data/ajout.json", reponse, (err) => {
-	 if (err) throw err;
-	 console.log('Terminé');
-	 });
-
-
- let data = fs.readFileSync('public/data/adresses.json');
- console.log("*********************************************");
- console.log(reponse);
- console.log("*********************************************");
- //copyData("public/data/ajout.json", "public/data/adresses.json");
-
- res.end();
+ res.end(JSON.stringify(reponse));
 
 })
-
-/*
-function copyData(source, destination) {
- fs.readFile(source, 'utf8',  (err, data) => {
- if (err) throw err;
- //Récupérer données du fichier dans lequel on veut ajouter
- let membres = fs.readFileSync('public/data/adresses.json');
-
- membres = JSON.parse(membres);
-console.log('++');
-console.log(data);
- membres.push(JSON.parse(data));
- console.log("*********************************************");
- console.log(membres);
- console.log(membres[3]);
- console.log("*********************************************");
-
- // Effectuer un traitement modifier data
-	 fs.writeFile (destination, membres, (err) => {
-	 if (err) throw err;
-	 console.log(membres);
-	 console.log('Terminé');
-	 });
- });
-}
-*/
-
-//data = JSON.parse(data);
-//data = JSON.stringify(data);
-//console.log(data);
 
 
 const transforme_en_tableau = (collection) =>
